48caea32e2584a422821c77b5b794f1c4a0df504
kubo-yasu-yk <yasuhiro.kubota0716ykyk@gmail.com>
2025-09-25 12:57:33 +0900

    トーストコンポーネントにおいて、トーストが閉じられた際に自動的に非表示にする機能を追加しました。

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -2,13 +2,13 @@ import { Toast, ToastClose, ToastDescription, ToastProvider, ToastTitle, ToastVi
 import { useToast } from './use-toast'
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts, dismiss } = useToast()
 
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...props} onOpenChange={(open) => !open && dismiss(id)}>
             <div className="grid gap-1">
               {title ? <ToastTitle>{title}</ToastTitle> : null}
               {description ? <ToastDescription>{description}</ToastDescription> : null}
